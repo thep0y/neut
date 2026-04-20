407fbbf56d4b97c836542e0abdd8908460b4ba51
thepoy <thepoy@163.com>
2026-04-20 12:52:26 +0800

    fix: SidebarMenuButton颜色错误

--- src/components/sidebar/SidebarMenuButton/SidebarMenuButton.styles.ts
+++ src/components/sidebar/SidebarMenuButton/SidebarMenuButton.styles.ts
@@ -1,13 +1,21 @@
 import { cva } from "class-variance-authority";
+import { clsx } from "~/lib/utils";
 
 export const sidebarMenuButtonVariants = cva(
-  "peer/menu-button group/menu-button flex w-full items-center gap-2 overflow-hidden rounded-md p-2 text-left text-sm ring-sidebar-ring outline-hidden transition-[width,height,padding] group-has-data-[sidebar=menu-action]/menu-item:pr-8 group-data-[collapsible=icon]:size-8! group-data-[collapsible=icon]:p-2! hover:bg-sidebar-accent hover:text-sidebar-accent-foreground focus-visible:ring-2 active:bg-sidebar-accent active:text-sidebar-accent-foreground disabled:pointer-events-none disabled:opacity-50 aria-disabled:pointer-events-none aria-disabled:opacity-50 data-open:hover:bg-sidebar-accent data-open:hover:text-sidebar-accent-foreground data-active:bg-sidebar-accent data-active:font-medium data-active:text-sidebar-accent-foreground [&_svg]:size-4 [&_svg]:shrink-0 [&>span:last-child]:truncate",
+  clsx(
+    "peer/menu-button group/menu-button flex w-full items-center gap-2 overflow-hidden rounded-md p-2 text-left text-sm outline-hidden transition-[width,height,padding] group-has-data-[sidebar=menu-action]/menu-item:pr-8 group-data-[collapsible=icon]:size-8! group-data-[collapsible=icon]:p-2! focus-visible:ring-2 disabled:pointer-events-none disabled:opacity-50 aria-disabled:pointer-events-none aria-disabled:opacity-50 data-[active=true]:font-medium [&_svg]:size-4 [&_svg]:shrink-0 [&>span:last-child]:truncate",
+    "ring-neutral-400 hover:bg-neutral-100 hover:text-neutral-900 active:bg-neutral-100 active:text-neutral-900 data-open:hover:bg-neutral-100 data-open:hover:text-neutral-900 data-[active=true]:bg-neutral-100 data-[active=true]:text-neutral-900",
+    "dark:ring-neutral-500 dark:hover:bg-neutral-800 dark:hover:text-neutral-50 dark:active:bg-neutral-800 dark:active:text-neutral-50 dark:data-open:hover:bg-neutral-800 dark:data-open:hover:text-neutral-50 dark:data-[active=true]:bg-neutral-800 dark:data-[active=true]:text-neutral-50",
+  ),
   {
     variants: {
       variant: {
-        ghost: "hover:bg-sidebar-accent hover:text-sidebar-accent-foreground",
-        outline:
-          "bg-background shadow-[0_0_0_1px_hsl(var(--sidebar-border))] hover:bg-sidebar-accent hover:text-sidebar-accent-foreground hover:shadow-[0_0_0_1px_hsl(var(--sidebar-accent))]",
+        ghost:
+          "hover:bg-neutral-100 hover:text-neutral-900 dark:hover:bg-neutral-800 dark:hover:text-neutral-50",
+        outline: clsx(
+          "bg-white shadow-[0_0_0_1px_(var(--color-neutral-200)] hover:bg-neutral-100 hover:text-neutral-900 hover:shadow-[0_0_0_1px_var(--color-neutral-100)]",
+          "dark:bg-neutral-950 dark:shadow-[0_0_0_1px_hsl(var(--sidebar-border))] dark:hover:bg-neutral-800 dark:hover:text-neutral-50 dark:hover:shadow-[0_0_0_1px_--alpha(var(--color-white) / 10%)]]",
+        ),
       },
       size: {
         md: "h-8 text-sm",
