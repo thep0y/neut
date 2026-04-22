7b99d9f76db376157f735a725c4574c060d202d5
thepoy <thepoy@163.com>
2026-04-22 15:36:22 +0800

    chore: 修改packages及打包入口

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,37 @@ import solidPlugin from "vite-plugin-solid";
 import devtools from "solid-devtools/vite";
 import dts from "vite-plugin-dts";
 import path from "node:path";
+import fs from "node:fs";
+
+const dirs = {
+  components: path.resolve(__dirname, "src/components"),
+  hooks: path.resolve(__dirname, "src/hooks"),
+  utils: path.resolve(__dirname, "src/utils"),
+};
+const entries: Record<string, string> = {
+  index: path.resolve(__dirname, "src/index.ts"),
+};
+
+for (const [dirName, dirPath] of Object.entries(dirs)) {
+  const entry = path.resolve(dirPath, "index.ts");
+  if (fs.existsSync(entry)) {
+    const key = `${dirName}/index`;
+    entries[key] = entry;
+  }
+
+  if (fs.existsSync(dirPath)) {
+    const folders = fs
+      .readdirSync(dirPath)
+      .filter((item) => fs.statSync(path.resolve(dirPath, item)).isDirectory());
+    for (const folder of folders) {
+      const indexPath = path.resolve(dirPath, folder, "index.ts");
+      if (fs.existsSync(indexPath)) {
+        const key = `${dirName}/${folder}/index`;
+        entries[key] = indexPath;
+      }
+    }
+  }
+}
 
 export default defineConfig({
   resolve: {
@@ -21,9 +52,11 @@ export default defineConfig({
     port: 5173,
   },
   build: {
+    sourcemap: true,
     target: "esnext",
     lib: {
-      entry: [path.resolve(__dirname, "src/index.ts")],
+      // entry: [path.resolve(__dirname, "src/index.ts")],
+      entry: entries,
       name: "@neut/ui",
       formats: ["es"],
     },
